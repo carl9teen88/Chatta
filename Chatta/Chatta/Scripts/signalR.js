fd43c01bd9b8b2b227be158937985ffeb2981414
dharl072810 <dharl072810@gmail.com>
2018-03-17 14:31:16 +0800

    Integrate signal R in a chat app.

---
+++ Chatta/Chatta/Scripts/signalR.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var server = $.connection.chatHub.server;
+    var client = $.connection.chatHub.client;
+
+    $.connection.hub.start()
+        .done(function () {
+            console.log("connected!");
+        })
+        .fail(function () {
+            alert("Failed to connect.")
+        });
+
+    client.send = function (message) {
+        var text = "<label class='pull-right' style='background-color: #99c199; color: white'>" + message +"</label><br/>";
+        $("#messages").append(text + "<br/>");
+    };
+
+    $("#sendNow").click(function () {
+        var message = $("#chatMessage").val();
+        var text = "<label class='pull-left' style='background-color: #757fa9; color: white'>" + message + "</label><br/>";
+        $("#messages").append(text);
+        server.send(message);
+    })
+})()
